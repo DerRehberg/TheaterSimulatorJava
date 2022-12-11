0e19c8a5dc5c4f57bb138f82f0e58b5fe5069c9f
Nageki Fujishiro <vacanickel@yahoo.de>
2022-12-11 13:52:34 +0100

    Update Main.java
    Added more Puppet Creation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,7 +43,7 @@ public class Main extends JFrame{
         ReadBackgrounds();
         ReadPuppets();
         Stick.init();
-        for(int i = 0; i<1; i++)
+        for(int i = 0; i<Puppet.Puppettexts.size(); i++)
         {
             Puppet.Puppets.add(new Puppet());
         }
